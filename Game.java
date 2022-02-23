28779e273e95f7f633242c49b40ccf3d0a806ee3
Rubi Renteria <53914397+rrente43@users.noreply.github.com>
2022-02-23 15:49:05 -0700

    Add files via upload
    Starting on assignment two for oop

---
+++ Game.java
@@ -0,0 +1,16 @@
+package HW2;
+public class Game {
+    //all the attributes
+    public String startTime;
+    public String endTime;
+
+ //constructor method
+    Public Game(String startTime, string endTime){
+
+    }
+    
+
+ //all the methods
+    
+}
+
---
+++ Player.java
@@ -0,0 +1,17 @@
+package HW2;
+
+public class Player {
+//all the attributes
+ public String name;
+ public int points;
+ public int numTorpedoes;
+ 
+ public Torpedo[] torpedoes;
+
+ //construbtor
+
+
+ //all the methods
+
+
+}
